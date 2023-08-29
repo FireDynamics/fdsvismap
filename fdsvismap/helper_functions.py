2fe2104519a9acdca9e60602f6cfdce4324ce71f
Kristian <73885850+kriboe90@users.noreply.github.com>
2023-08-29 09:55:39 -0400

    add docstrings

--- fdsvismap/helper_functions.py
+++ fdsvismap/helper_functions.py
@@ -1,9 +1,14 @@
 import numpy as np
 
 def find_closest_point(coord_array, value):
-    '''
-    :param coord_array: FDS meshgrid with coordinates
-    :param value: coordinate of waypoint
-    :return: closest coordinate in FDS meshgrid to given value
-    '''
+    """
+    Find the closest point in a given coordinate array to a given value.
+
+    :param coord_array: The FDS meshgrid containing coordinates.
+    :type coord_array: numpy.ndarray
+    :param value: The coordinate of the waypoint.
+    :type value: float
+    :return: The index of the closest coordinate in the FDS meshgrid to the given value.
+    :rtype: int
+    """
     return (np.abs(coord_array - value)).argmin()
