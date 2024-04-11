0640ccf95af0f8ea39d590038b8764c7972db759
Kristian <boerger.k@icloud.com>
2024-04-11 14:31:11 +0200

    update room_fire example

--- examples/room_fire/room_fire.py
+++ examples/room_fire/room_fire.py
@@ -1,26 +1,30 @@
 # Import fdsvismap module
 from fdsvismap import VisMap
-
+import matplotlib.pyplot as plt
 # Set path for FDS simulation directory and background image
 sim_dir = 'fds_data'
-bg_img = 'misc/floorplan.png'
-# Create instance of VisMap class
-vis = VisMap(sim_dir, max_vis=30, min_vis=10)
+bg_img = 'misc/floorplan.png' # Create instance of VisMap class
+vis = VisMap(sim_dir, max_vis=30, min_vis=0)
+vis.num_edge_cells =1
 
 # Add background image
 vis.add_background_image(bg_img) #sad
 
 # Set starpoint and waypoints along escape route
 vis.set_start_point(1, 1)
-vis.set_waypoint(8, 5, 3, 180)
-vis.set_waypoint(9.8, 6, 3, 270)
-vis.set_waypoint(17, 0, 3, 0)
+vis.set_waypoint(8, 5, 8, 180)
+vis.set_waypoint(9.8, 6, 8, 270)
+vis.set_waypoint(17, 0, 8, 0)
 
 # Set times when the simulation should be evaluated
-vis.set_times([0, 60, 120])
+times = range(0, 500, 50)
+vis.set_times(times)
 
 # Do the required calculations to create the Vismap
-vis.compute_all()
+vis.compute_all(view_angle=True)
+aset_map = vis.get_aset_map()
 
-# Plot time agglomerated absolute boolean vismap
-vis.plot_time_aggl_abs_bool_vismap()
+# # Plot time agglomerated absolute boolean vismap
+fig, ax = vis.create_time_aggl_abs_bool_vismap()
+# fix, ax = vis.create_aset_map_plot()
+plt.show()
